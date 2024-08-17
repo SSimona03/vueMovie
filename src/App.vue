5ac801f067732f201cc709c2895b8b1d18af0bb8
ssimona03 <sstroe92@gmail.com>
2024-08-17 19:18:43 +0100

    wip

--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,15 @@
 </script>
 
 <template>
- 
-
 
 
   <main class="p-10 ">
-
-    <div class="wrapper">
+    <div class="wrapper p-10">
       HELLO
     </div>
   </main>
 </template>
 
 <style scoped>
-
 /* https://api.themoviedb.org/3/configuration */
 </style>
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      './pages/**/*.{js,ts,jsx,tsx,mdx,vue}',
+      './components/**/*.{js,ts,jsx,tsx,mdx,vue}',
+      './app/**/*.{js,ts,jsx,tsx,mdx,vue}',
+      './app/*.{js,ts,jsx,tsx,mdx,vue}',
+      './components/**/*.{js,ts,jsx,tsx,mdx,vue}'
+    ],
+    theme: {
+      extend: {
+       
+      },
+    },
+    plugins: [],
+  }
